f0f450a38e18964cbbeb9d1cfa8d420be3a91be6
uschtwill <uschtrinw@gmail.com>
2022-11-16 17:34:55 +0000

    feat: better header

--- packages/header/src/Header.tsx
+++ packages/header/src/Header.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export function Header() {
     return (
         <header>
-            Lerna is the original monorepo tool!
+            Much better header!
         </header>
     );
 }
