58224991e35b62c8e1110afa5048233d9af45998
uschtwill <uschtrinw@gmail.com>
2022-11-24 12:24:31 +0000

    feat: amazing feature

--- packages/footer/src/Footer.tsx
+++ packages/footer/src/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export function Footer() {
     return (
         <footer>
-            Another Even Better Footer!!!!!
+            Good Footer.
         </footer>
     );
 }
